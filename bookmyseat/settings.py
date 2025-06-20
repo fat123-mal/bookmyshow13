9a3f2345c2cb7359323440a7a5cb67a6907e54f2
Fathima <pathuf580@gmail.com>
2025-06-21 00:15:07 +0530

    Push working Django app for Render deploy

--- bookmyseat/settings.py
+++ bookmyseat/settings.py
@@ -28,8 +28,7 @@ SECRET_KEY = 'django-insecure-c8aetlj(=vp90n@#yoc^&d(_6ivp(d!bv-4-f!r$lawptjzrwu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
-
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="localhost").split(",")
 
 
 # Application definition
